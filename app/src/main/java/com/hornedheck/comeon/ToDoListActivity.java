821261bd6dddb2fe2699628a11a365da4c32e963
Horned Heck <vitaliauseitsau@gmail.com>
2018-10-22 20:25:34 +0300

    TDA Toolbar created

--- app/src/main/java/com/hornedheck/comeon/ToDoListActivity.java
+++ app/src/main/java/com/hornedheck/comeon/ToDoListActivity.java
@@ -1,7 +1,10 @@
 package com.hornedheck.comeon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ToDoListActivity
   extends AppCompatActivity{
@@ -10,5 +13,13 @@ public class ToDoListActivity
 	protected void onCreate( Bundle savedInstanceState ){
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_to_do_list );
+		ImageButton settingsButton = findViewById( R.id.settings_button );
+		settingsButton.setOnClickListener( new View.OnClickListener(){
+			
+			@Override
+			public void onClick( View v ){
+				startActivity( new Intent( ToDoListActivity.this , SettingsActivity.class ) );
+			}
+		} );
 	}
 }
