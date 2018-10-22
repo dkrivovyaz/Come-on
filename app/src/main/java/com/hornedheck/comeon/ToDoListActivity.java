d847d8cc8577f41e3cc90cc24d224488415e685c
Horned Heck <vitaliauseitsau@gmail.com>
2018-10-22 19:30:49 +0300

    create

---
+++ app/src/main/java/com/hornedheck/comeon/ToDoListActivity.java
@@ -0,0 +1,14 @@
+package com.hornedheck.comeon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ToDoListActivity
+  extends AppCompatActivity{
+	
+	@Override
+	protected void onCreate( Bundle savedInstanceState ){
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_to_do_list );
+	}
+}
