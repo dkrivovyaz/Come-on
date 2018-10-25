61c7f7e652a61da3d490d207701b7c3f955ed276
Casper133 <133casper@gmail.com>
2018-10-25 16:58:14 +0300

    Изменен формат времени на более привычный (24-х часовой)

--- app/src/main/java/com/hornedheck/comeon/SettingsActivity.java
+++ app/src/main/java/com/hornedheck/comeon/SettingsActivity.java
@@ -6,12 +6,12 @@ import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 public class SettingsActivity
   extends AppCompatActivity {
@@ -64,38 +64,39 @@ public class SettingsActivity
 
     private void setInitialStartDayTime() {
 
-	    //считывание start time
+	    // Считывание startTime и вывод его на экран:
 
-        currentStartTime.setText(DateUtils.formatDateTime(this,
-                time.getTimeInMillis(),
-                DateUtils.FORMAT_SHOW_TIME));
+        int hours = time.get(Calendar.HOUR_OF_DAY);
+        int minutes = time.get(Calendar.MINUTE);
+        String startTime = String.format(Locale.US, "%02d:%02d", hours, minutes);
+        currentStartTime.setText(startTime);
 
-        //сохранение start time в preferences
+        // Сохранение startTime в preferences:
 
         final String START_TIME = "startTime";
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         Editor editor = preferences.edit();
-        editor.putString(START_TIME, String.valueOf(DateUtils.formatDateTime(this,time.getTimeInMillis(), DateUtils.FORMAT_SHOW_TIME)));
+        editor.putString(START_TIME, String.valueOf(startTime));
         editor.apply();
     }
 
     private void setInitialEndDayTime() {
 
-	    //считывание end time
+	    // Считывание endTime и вывод его на экран:
 
-        currentEndTime.setText(DateUtils.formatDateTime(this,
-                time.getTimeInMillis(),
-                DateUtils.FORMAT_SHOW_TIME));
+        int hours = time.get(Calendar.HOUR_OF_DAY);
+        int minutes = time.get(Calendar.MINUTE);
+        String endTime = String.format(Locale.US, "%02d:%02d", hours, minutes);
+        currentEndTime.setText(endTime);
 
-        //сохранение end time в preferences
+        // Сохранение endTime в preferences:
 
         final String END_TIME = "endTime";
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         Editor editor = preferences.edit();
-        editor.putString(END_TIME, String.valueOf(DateUtils.formatDateTime(this,time.getTimeInMillis(), DateUtils.FORMAT_SHOW_TIME)));
+        editor.putString(END_TIME, String.valueOf(endTime));
         editor.apply();
     }
-
 
 
     // Обработка выбора нового времени в TimePicker-диалоге:
